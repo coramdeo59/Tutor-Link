b126239101426c756c4a60bdf4085f943547cb43
coramdeo59 <melkamuelias59@gmail.com>
2025-03-01 16:59:05 +0300

    using pnpm reinstall backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { PaymentsModule } from './payments/payments.module';
+import { SessionsModule } from './sessions/sessions.module';
+import { StudentsModule } from './students/students.module';
+import { TutorsModule } from './tutors/tutors.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, TutorsModule, StudentsModule, SessionsModule, PaymentsModule, FeedbackModule, NotificationsModule, AdminModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
