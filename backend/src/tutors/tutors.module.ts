9a56554f3d4f03bb15c050cfd68d65c5b5d40b08
coramdeo59 <melkamuelias59@gmail.com>
2025-03-01 16:44:34 +0300

    remove npm to install pnpm

--- backend/src/tutors/tutors.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TutorsService } from './tutors.service';
-import { TutorsController } from './tutors.controller';
-
-@Module({
-  controllers: [TutorsController],
-  providers: [TutorsService],
-})
-export class TutorsModule {}
