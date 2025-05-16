dd355574f0034bc40c8309e926e4fd4f13b6e546
Test User <test@example.com>
2025-05-16 17:53:25 +0300

    chore(main): update app bootstrap logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { json } from 'body-parser'; // Import body-parser json middleware
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Increase the JSON payload limit (e.g., to 5MB)
-  // Adjust the limit ('5mb') as needed for your data size
-  app.use(json({ limit: '5mb' }));
+  app.enableCors({
+    origin: 'http://localhost:4001', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-
-  // Swagger configuration
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Tutor‑Link API')
-    .setDescription('Tutor‑Link backend API documentation')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, swaggerDoc);
 
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   const host = `0.0.0.0`;
